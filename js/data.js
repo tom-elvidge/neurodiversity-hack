85b18e72b7823b42bf2fbcc392964ea27d92c4dd
Tom Elvidge <tee504@york.ac.uk>
2021-03-26 15:54:01 +0000

    Added function to plot bar chart

---
+++ js/data.js
@@ -0,0 +1,14 @@
+labelData = [
+    {
+        label: "label here",
+        val: 50
+    },
+    {
+        label: "another label",
+        val: 31
+    },
+    {
+        label: "label",
+        val: 10
+    }
+]
--- js/plot-data.js
+++ js/plot-data.js
@@ -1,16 +1,48 @@
-// var data = [30, 86, 168, 281, 303, 365];
-// d3.select(".chart")
-//     .selectAll("div")
-//     .data(data)
-//     .enter()
-//     .append("div")
-//     .style("width", function (d) { return d + "px"; })
-//     .text(function (d) { return d; });
-
-// d3.create("span")
-//     .style("color", "white")
-//     .style("background-color", "black")
-//     .html("Hello, world!")
-//     .node();
 d3.select("p").style("color", "green");
 d3.select("body").append("p").text("Third paragraph.");
+
+function plotBars(data, divId, width, height) {
+    // https://datawanderings.com/2019/10/20/tutorial-advanced-bar-chart-in-d3-js-v-5/
+    // Init SVG
+    var margin = 5;
+    var padding = 5;
+    var adj = 20;
+    var svg = d3.select(divId).append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "-" + adj + " -" + adj + " " + (width + adj) + " " + (height + adj * 2))
+        .style("padding", padding)
+        .style("margin", margin)
+        .classed("svg-content", true);
+    // Scales
+    var xScale = d3.scaleBand()
+        .rangeRound([0, width])
+        .paddingInner(0.05);
+    var yScale = d3.scaleLinear()
+        .rangeRound([0, height]);
+    xScale.domain(data.map(function (d) { return d.label }))
+    yScale.domain([d3.max(data, function (d) { return d.val; }), 0]);
+    // Axes
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+    svg.append("g")
+        .attr("class", "axis")
+        .call(d3.axisLeft(yScale));
+    // Add data
+    svg.selectAll("div")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+            return xScale(d.label);
+        })
+        .attr("y", function (d) {
+            return yScale(d.val);
+        })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function (d) {
+            return height - yScale(d.val);
+        });
+}
