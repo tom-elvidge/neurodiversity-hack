07975d492ff9cf28740e799f7e3fbb3dc6fd7da9
David Swarbrick <david.swarbrick9@gmail.com>
2021-03-26 14:29:31 +0000

    Append and colour change

--- js/plot-data.js
+++ js/plot-data.js
@@ -1,4 +1,4 @@
-var data = [30, 86, 168, 281, 303, 365];
+// var data = [30, 86, 168, 281, 303, 365];
 // d3.select(".chart")
 //     .selectAll("div")
 //     .data(data)
@@ -7,8 +7,10 @@ var data = [30, 86, 168, 281, 303, 365];
 //     .style("width", function (d) { return d + "px"; })
 //     .text(function (d) { return d; });
 
-d3.create("span")
-    .style("color", "white")
-    .style("background-color", "black")
-    .html("Hello, world!")
-    .node()
+// d3.create("span")
+//     .style("color", "white")
+//     .style("background-color", "black")
+//     .html("Hello, world!")
+//     .node();
+d3.select("p").style("color", "green");
+d3.select("body").append("p").text("Third paragraph.");
