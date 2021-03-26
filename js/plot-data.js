4fd80e4dcb68166176e21f827c36c4fb5ef86a50
David Swarbrick <david.swarbrick9@gmail.com>
2021-03-26 13:47:45 +0000

    Following https://scrimba.com/scrim/cast-1953?pl=pEKMsN

---
+++ js/plot-data.js
@@ -0,0 +1,14 @@
+var data = [30, 86, 168, 281, 303, 365];
+// d3.select(".chart")
+//     .selectAll("div")
+//     .data(data)
+//     .enter()
+//     .append("div")
+//     .style("width", function (d) { return d + "px"; })
+//     .text(function (d) { return d; });
+
+d3.create("span")
+    .style("color", "white")
+    .style("background-color", "black")
+    .html("Hello, world!")
+    .node()
